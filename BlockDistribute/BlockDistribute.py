0d38c5c49d5a9c57fc034804d0a3d4e5ebb87851
Wouter Glorieux <wouter@valyrian.tech>
2016-08-27 14:52:00 +0200

    pep8

--- BlockDistribute/BlockDistribute.py
+++ BlockDistribute/BlockDistribute.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 from validators import validators as validator
 from BIP44 import BIP44 as BIP44
 import BlockData.BlockData as BlockData
@@ -21,8 +20,9 @@ from google.appengine.api import urlfetch
 urlfetch.set_default_fetch_deadline(60)
 
 
-REQUIRED_CONFIRMATIONS = 3 #must be at least 3
-TRANSACTION_FEE = 10000 #in Satoshis
+REQUIRED_CONFIRMATIONS = 3  # must be at least 3
+TRANSACTION_FEE = 10000  # in Satoshis
+
 
 def getNextIndex():
     distributers_query = datastore.Distributer.query(ancestor=datastore.distributers_key()).order(-datastore.Distributer.walletIndex)
@@ -35,44 +35,38 @@ def getNextIndex():
 
     return i
 
+
 def distributerToDict(distributer):
-    distributerDict = {}
+    distributer_dict = {'name': distributer.key.id(),
+                        'address': distributer.address,
+                        'distributionSource': distributer.distributionSource,
+                        'registrationAddress': distributer.registrationAddress,
+                        'registrationBlockHeight': distributer.registrationBlockHeight,
+                        'registrationXPUB': distributer.registrationXPUB,
+                        'distribution': distributer.distribution,
+                        'minimumAmount': distributer.minimumAmount,
+                        'threshold': distributer.threshold,
+                        'status': distributer.status,
+                        'visibility': distributer.visibility,
+                        'description': distributer.description,
+                        'creator': distributer.creator,
+                        'creatorEmail': distributer.creatorEmail,
+                        'youtube': distributer.youtube,
+                        'feeAddress': distributer.feeAddress,
+                        'feePercent': distributer.feePercent,
+                        'maxTransactionFee': distributer.maxTransactionFee,
+                        'date': int(time.mktime(distributer.date.timetuple()))}
 
-    distributerDict['name'] = distributer.key.id()
-    distributerDict['address'] = distributer.address
-
-    distributerDict['distributionSource'] = distributer.distributionSource
-    distributerDict['registrationAddress'] = distributer.registrationAddress
-    distributerDict['registrationBlockHeight'] = distributer.registrationBlockHeight
-    distributerDict['registrationXPUB'] = distributer.registrationXPUB
-    distributerDict['distribution'] = distributer.distribution
-
-    distributerDict['minimumAmount'] = distributer.minimumAmount
-    distributerDict['threshold'] = distributer.threshold
-
-    distributerDict['status'] = distributer.status
-    distributerDict['visibility'] = distributer.visibility
-
-    distributerDict['description'] = distributer.description
-    distributerDict['creator'] = distributer.creator
-    distributerDict['creatorEmail'] = distributer.creatorEmail
-    distributerDict['youtube'] = distributer.youtube
-
-    distributerDict['feeAddress'] = distributer.feeAddress
-    distributerDict['feePercent'] = distributer.feePercent
-
-    distributerDict['maxTransactionFee'] = distributer.maxTransactionFee
-
-    distributerDict['date'] = int(time.mktime(distributer.date.timetuple()))
-
-    return distributerDict
+    return distributer_dict
 
 
 def getDistributers():
     response = {'success': 0}
     distributers = []
 
-    distributers_query = datastore.Distributer.query(datastore.Distributer.visibility == 'Public', datastore.Distributer.status == 'Active', ancestor=datastore.distributers_key()).order(-datastore.Distributer.date)
+    distributers_query = datastore.Distributer.query(datastore.Distributer.visibility == 'Public',
+                                                     datastore.Distributer.status == 'Active',
+                                                     ancestor=datastore.distributers_key()).order(-datastore.Distributer.date)
     data = distributers_query.fetch()
     for distributer in data:
         distributers.append(distributerToDict(distributer))
@@ -82,9 +76,9 @@ def getDistributers():
 
     return response
 
+
 class Distributer():
     @ndb.transactional(xg=True)
-
     def __init__(self, name):
         self.error = ''
         if isinstance(name, (str, unicode)) and len(name) > 0:
@@ -105,32 +99,30 @@ class Distributer():
 
         return response
 
-
     def checkAddress(self, address):
         response = {'success': 0}
         if self.error == '':
             distributer = datastore.Distributer.get_by_id(self.name, parent=datastore.distributers_key())
 
             if distributer:
-                distributingRelation = {'relation': 'unrelated address'}
+                distributing_relation = {'relation': 'unrelated address'}
                 if distributer.address == address:
-                    distributingRelation['relation'] = 'distributing address'
+                    distributing_relation['relation'] = 'distributing address'
                 else:
                     share = 0
                     for recipient in distributer.distribution:
                         if recipient[0] == address:
-                            distributingRelation['relation'] = 'receiving address'
+                            distributing_relation['relation'] = 'receiving address'
                             if len(recipient) >= 3:
                                 share += recipient[2]
 
-                    if distributingRelation['relation'] == 'receiving address':
-                        distributingRelation['share'] = share
+                    if distributing_relation['relation'] == 'receiving address':
+                        distributing_relation['share'] = share
 
                     if distributer.registrationAddress == address and distributer.distributionSource in ['SIL', 'LBL', 'LRL', 'LSL']:
-                        distributingRelation['relation'] = 'registration address'
+                        distributing_relation['relation'] = 'registration address'
 
-
-                response[address] = distributingRelation
+                response[address] = distributing_relation
                 response['success'] = 1
 
             else:
@@ -138,9 +130,9 @@ class Distributer():
 
         return response
 
-
-
-    def saveDistributer(self, settings={}):
+    def saveDistributer(self, settings=None):
+        if not settings:
+            settings = {}
         response = {'success': 0}
 
         if self.error == '':
@@ -150,7 +142,6 @@ class Distributer():
                 distributer.distributionSource = settings['distributionSource']
             elif 'distributionSource' in settings:
                 self.error = 'Invalid distributionSource'
-
 
             if 'registrationAddress' in settings and (validator.validAddress(settings['registrationAddress']) or settings['registrationAddress'] == ''):
                 distributer.registrationAddress = settings['registrationAddress']
@@ -162,12 +153,10 @@ class Distributer():
             elif 'registrationXPUB' in settings:
                 self.error = 'Invalid registrationXPUB'
 
-
             if 'registrationBlockHeight' in settings and (validator.validBlockHeight(settings['registrationBlockHeight'])):
                 distributer.registrationBlockHeight = settings['registrationBlockHeight']
             elif 'registrationBlockHeight' in settings:
                 self.error = 'Invalid registrationBlockHeight: ' + str(settings['registrationBlockHeight'])
-
 
             if 'distribution' in settings and validator.validDistribution(eval(settings['distribution'])):
                 distributer.distribution = eval(settings['distribution'])
@@ -175,7 +164,6 @@ class Distributer():
                 distributer.distribution = []
             elif 'distribution' in settings:
                 self.error = 'Invalid distribution: ' + settings['distribution']
-
 
             if 'minimumAmount' in settings and validator.validAmount(settings['minimumAmount']):
                 distributer.minimumAmount = settings['minimumAmount']
@@ -187,7 +175,6 @@ class Distributer():
             elif 'threshold' in settings:
                 self.error = 'threshold must be a positive integer or equal to 0 (in Satoshis)'
 
-
             if 'status' in settings and settings['status'] in ['Pending', 'Active', 'Disabled']:
                 distributer.status = settings['status']
             elif 'status' in settings:
@@ -198,12 +185,10 @@ class Distributer():
             elif 'visibility' in settings:
                 self.error = 'visibility must be Public or Private'
 
-
             if 'description' in settings and validator.validDescription(settings['description']):
                 distributer.description = settings['description']
             elif 'description' in settings:
                 self.error = 'Invalid description'
-
 
             if 'creator' in settings and validator.validCreator(settings['creator']):
                 distributer.creator = settings['creator']
@@ -220,7 +205,6 @@ class Distributer():
             elif 'youtube' in settings:
                 self.error = 'Invalid youtube video ID'
 
-
             if 'feePercent' in settings and validator.validPercentage(settings['feePercent']):
                 distributer.feePercent = settings['feePercent']
             elif 'feePercent' in settings:
@@ -231,12 +215,10 @@ class Distributer():
             elif 'feeAddress' in settings:
                 self.error = 'Invalid feeAddress'
 
-
             if 'maxTransactionFee' in settings and validator.validAmount(settings['maxTransactionFee']):
                 distributer.maxTransactionFee = settings['maxTransactionFee']
             elif 'maxTransactionFee' in settings:
                 self.error = 'maxTransactionFee must be a positive integer or equal to 0 (in Satoshis)'
-
 
             if 'addressType' in settings and settings['addressType'] in ['PrivKey', 'BIP44']:
                 distributer.addressType = settings['addressType']
@@ -263,12 +245,10 @@ class Distributer():
             if not validator.validAddress(distributer.address):
                 self.error = 'Unable to get address for distributer'
 
-
             if self.error == '':
                 distributer.put()
                 response['distributer'] = distributerToDict(distributer)
                 response['success'] = 1
-
             else:
                 response['error'] = self.error
 
@@ -288,8 +268,6 @@ class Distributer():
 
         return response
 
-
-
     def updateDistribution(self):
         response = {'success': 0}
 
@@ -302,13 +280,14 @@ class Distributer():
                     SIL_data = BlockInputs.SIL(distributer.registrationAddress, distributer.registrationBlockHeight)
                     if 'success' in SIL_data and SIL_data['success'] == 1:
                         distribution = SIL_data['SIL']
-
                     else:
                         self.error = 'Unable to retrieve SIL'
 
                 elif distributer.distributionSource in ['LBL', 'LRL', 'LSL']:
-                    linker = BlockLinker.BlockLinker(distributer.registrationAddress, distributer.registrationXPUB, distributer.registrationBlockHeight)
-
+                    linker = BlockLinker.BlockLinker(distributer.registrationAddress,
+                                                     distributer.registrationXPUB,
+                                                     distributer.registrationBlockHeight)
+                    linker_data = {}
                     if distributer.distributionSource == 'LBL':
                         linker_data = linker.LBL()
                     elif distributer.distributionSource == 'LRL':
@@ -318,7 +297,6 @@ class Distributer():
 
                     if 'success' in linker_data and linker_data['success'] == 1:
                         distribution = linker_data[distributer.distributionSource]
-
                     else:
                         self.error = 'Unable to retrieve ' + distributer.distributionSource
 
@@ -327,7 +305,6 @@ class Distributer():
                     distributer.put()
                 else:
                     self.error = 'Invalid distribution'
-
 
             else:
                 response['error'] = 'No distributer with that name found.'
@@ -339,8 +316,6 @@ class Distributer():
             response['error'] = self.error
 
         return response
-
-
 
 
 class DoDistributing():
@@ -359,27 +334,22 @@ class DoDistributing():
             for distributer in distributers:
                 self.run(distributer)
 
-
-
-
-
     def run(self, distributer):
         success = False
         utxos_data = BlockData.utxos(distributer.address)
         if 'success' in utxos_data and utxos_data['success'] == 1:
-            UTXOs = utxos_data['UTXOs']
+            utxos = utxos_data['UTXOs']
         else:
             self.error = 'Unable to retrieve UTXOs'
 
+        total_input_value = 0
+        for utxo in utxos:
+            total_input_value += utxo['value']
 
-        totalInputValue = 0
-        for UTXO in UTXOs:
-            totalInputValue += UTXO['value']
-
-
-        if totalInputValue >= distributer.threshold:
-            logging.info('Starting distribution of ' + str(totalInputValue) + ' Satoshis, minimum output value: ' + str(distributer.minimumAmount))
-
+        if total_input_value >= distributer.threshold:
+            logging.info(
+                'Starting distribution of {0} Satoshis, minimum output value: {1}'.format(str(total_input_value),
+                                                                                          str(distributer.minimumAmount)))
 
             if distributer.distributionSource in ['SIL', 'LBL', 'LRL', 'LSL']:
                 distribution_data = Distributer(distributer.key.id()).updateDistribution()
@@ -392,75 +362,72 @@ class DoDistributing():
 
             if validator.validDistribution(distribution):
                 logging.info("distribution: " + str(distribution))
-                optimalOutputs = self.optimalOutputs(totalInputValue, distribution, distributer)
-                logging.info("optimal outputs: " + str(optimalOutputs))
+                optimal_outputs = self.optimalOutputs(total_input_value, distribution, distributer)
+                logging.info("optimal outputs: " + str(optimal_outputs))
 
-                privKeys = {}
+                private_keys = {}
                 if distributer.addressType == 'PrivKey':
-                    privKeys = {distributer.address: distributer.privateKey}
+                    private_keys = {distributer.address: distributer.privateKey}
 
                 elif distributer.addressType == 'BIP44':
-                    privKeys = datastore.get_service_private_key(datastore.Services.BlockDistribute, distributer.walletIndex)
+                    private_keys = datastore.get_service_private_key(datastore.Services.BlockDistribute, distributer.walletIndex)
 
                 if distributer.distributionSource == 'SIL' and distributer.address == distributer.registrationAddress:
                     self.error = 'Dark magic detected! Ponzi schemes are illegal!!'
 
-                if len(optimalOutputs) > 0 and self.error == '':
-                    totalOutputValue = 0
-                    for tx_output in optimalOutputs:
-                        totalOutputValue += tx_output['value']
+                if len(optimal_outputs) > 0 and self.error == '':
+                    total_output_value = 0
+                    for tx_output in optimal_outputs:
+                        total_output_value += tx_output['value']
 
-                    totalFee = totalInputValue - totalOutputValue
-                    logging.info("Sending " + str(totalInputValue) + ' Satoshis to ' + str(len(optimalOutputs)) + ' recipients with a total fee of ' + str(totalFee))
-                    tx = TxFactory.makeCustomTransaction(privKeys, UTXOs, optimalOutputs, totalFee)
-                    if tx != None:
+                    total_fee = total_input_value - total_output_value
+                    logging.info(
+                        "Sending {0} Satoshis to {1} recipients with a total fee of {2}".format(str(total_input_value),
+                                                                                                str(len(optimal_outputs)),
+                                                                                                str(total_fee)))
+                    tx = TxFactory.makeCustomTransaction(private_keys, utxos, optimal_outputs, total_fee)
+                    if tx is not None:
                         TxFactory.sendTransaction(tx)
                 else:
                     logging.error(self.error)
 
-
-                if success == True:
+                if success:
                     logging.info("Success")
                 else:
                     logging.error("Failed to send transaction")
-
-
-
 
             else:
                 self.error = 'Invalid distribution: ' + str(distribution)
 
         return success
 
-
     def optimalOutputs(self, amount, distribution, distributer):
         optimal = []
-        valueToDistribute = amount-distributer.maxTransactionFee
+        value_to_distribute = amount-distributer.maxTransactionFee
 
         if distributer.feePercent != 0 and distributer.feeAddress != '':
-            distributingFee = int(valueToDistribute * (distributer.feePercent / 100.0))
-            if distributingFee < 10000:
-                distributingFee = 10000
+            distributing_fee = int(value_to_distribute * (distributer.feePercent / 100.0))
+            if distributing_fee < 10000:
+                distributing_fee = 10000
 
-            valueToDistribute -= distributingFee
-            optimal.append({'address': distributer.feeAddress, 'value': distributingFee})
+            value_to_distribute -= distributing_fee
+            optimal.append({'address': distributer.feeAddress, 'value': distributing_fee})
 
-
-        if valueToDistribute < distributer.minimumAmount:
+        if value_to_distribute < distributer.minimumAmount:
             self.error = 'minimumAmount is lower than the amount available to distribute.'
 
-        sortedDistribution = sorted(distribution, key=lambda  x: x[1], reverse=True)
+        sorted_distribution = sorted(distribution, key=lambda x: x[1], reverse=True)
 
-        for i in range(len(sortedDistribution)-1, -1, -1):
-            tmpTotal = 0
-            for j in range(0, len(sortedDistribution)):
-                tmpTotal += sortedDistribution[j][1]
+        for i in range(len(sorted_distribution)-1, -1, -1):
+            tmp_total = 0
+            for j in range(0, len(sorted_distribution)):
+                tmp_total += sorted_distribution[j][1]
 
-            share = sortedDistribution[i][1]/float(tmpTotal)
+            share = sorted_distribution[i][1]/float(tmp_total)
 
-            if share*valueToDistribute < distributer.minimumAmount:
-                del sortedDistribution[i]
+            if share*value_to_distribute < distributer.minimumAmount:
+                del sorted_distribution[i]
             else:
-                optimal.append({'address': sortedDistribution[i][0], 'value': int(share*valueToDistribute)})
+                optimal.append({'address': sorted_distribution[i][0], 'value': int(share*value_to_distribute)})
 
         return optimal
