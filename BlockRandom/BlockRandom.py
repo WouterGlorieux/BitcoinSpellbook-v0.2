015c3b7e527ad7e882921ebc98d424f969eaeb1a
Wouter Glorieux <wouter@valyrian.tech>
2016-08-27 21:17:45 +0200

    pep8

--- BlockRandom/BlockRandom.py
+++ BlockRandom/BlockRandom.py
@@ -6,7 +6,7 @@ from BlockLinker import BlockLinker
 
 
 class Random():
-    def __init__(self, address='', blockHeight=0, xpub=''):
+    def __init__(self, address='', block_height=0, xpub=''):
         self.error = ''
         self.address = ''
         self.xpub = ''
@@ -16,8 +16,8 @@ class Random():
         else:
             self.address = address
 
-        if isinstance(blockHeight, int) and blockHeight >= 0:
-            self.blockHeight = blockHeight
+        if isinstance(block_height, int) and block_height >= 0:
+            self.block_height = block_height
         else:
             self.error = 'block_height must be an integer greater than or equal to zero'
 
@@ -26,9 +26,9 @@ class Random():
         else:
             self.xpub = xpub
 
-    def fromBlock(self, rngBlockHeight=0):
+    def fromBlock(self, rng_block_height=0):
         response = {'success': 0}
-        random = self.RNG(rngBlockHeight)
+        random = self.RNG(rng_block_height)
 
         if self.error == '':
             response['random'] = random
@@ -38,10 +38,10 @@ class Random():
 
         return response
 
-    def proportionalRandom(self, source, rngBlockHeight=0):
+    def proportionalRandom(self, source, rng_block_height=0):
         response = {'success': 0}
         distribution = self.getDistribution(source)
-        rand = self.RNG(rngBlockHeight)
+        rand = self.RNG(rng_block_height)
         winner = self.winner(distribution, rand)
 
         if self.error == '':
@@ -52,50 +52,47 @@ class Random():
 
         return response
 
-
-    def RNG(self, rngBlockHeight=0):
+    def RNG(self, rng_block_height=0):
         rand = 0.0
 
-        if rngBlockHeight != 0:
-            block_data = BlockData.block(rngBlockHeight)
+        if rng_block_height != 0:
+            block_data = BlockData.block(rng_block_height)
         else:
             block_data = BlockData.latestBlock()
 
         if 'success' in block_data and block_data['success'] == 1:
-            if rngBlockHeight != 0:
-                rngHash = block_data['block']['hash']
+            if rng_block_height != 0:
+                rng_hash = block_data['block']['hash']
             else:
-                rngHash = block_data['latestBlock']['hash']
+                rng_hash = block_data['latestBlock']['hash']
 
-            intHash = int(rngHash, 16)
+            int_hash = int(rng_hash, 16)
 
-            strFloat = '0.'
-            for i in range(len(str(intHash))-1, -1, -1):
-                strFloat += str(intHash)[i]
+            str_float = '0.'
+            for i in range(len(str(int_hash))-1, -1, -1):
+                str_float += str(int_hash)[i]
 
-            rand = float(strFloat)
+            rand = float(str_float)
 
         else:
             self.error = 'Unable to retrieve block for rng'
 
         return rand
 
-
     def getDistribution(self, source):
         distribution = []
         if self.error == '':
             data = {}
             if source == 'SIL':
-                data = BlockInputs.SIL(self.address, self.blockHeight)
+                data = BlockInputs.SIL(self.address, self.block_height)
             elif source == 'LBL':
-                data = BlockLinker.BlockLinker(self.address, self.xpub, self.blockHeight).LBL()
+                data = BlockLinker.BlockLinker(self.address, self.xpub, self.block_height).LBL()
             elif source == 'LRL':
-                data = BlockLinker.BlockLinker(self.address, self.xpub, self.blockHeight).LRL()
+                data = BlockLinker.BlockLinker(self.address, self.xpub, self.block_height).LRL()
             elif source == 'LBL':
-                data = BlockLinker.BlockLinker(self.address, self.xpub, self.blockHeight).LSL()
+                data = BlockLinker.BlockLinker(self.address, self.xpub, self.block_height).LSL()
             else:
                 self.error = 'Unknown distribution source'
-
 
             if 'success' in data and data['success'] == 1:
                 distribution = data[source]
@@ -106,31 +103,28 @@ class Random():
         return distribution
 
     def addCumulative(self, distribution):
-        cumul = 0
+        cumulative = 0
         for i in range(0, len(distribution)):
-            cumul += distribution[i][1]
-            distribution[i].append(cumul)
+            cumulative += distribution[i][1]
+            distribution[i].append(cumulative)
 
         return distribution
 
-
     def winner(self, distribution, rand):
-        nDistribution = len(distribution)
+        n_distribution = len(distribution)
 
-        winnerIndex = -1
-        winnerAddress = ''
-        totalValue = 0
-        if nDistribution > 0:
+        winner_index = -1
+        winner_address = ''
+        total_value = 0
+        if n_distribution > 0:
             values = self.extractValues(distribution)
-            totalValue = sum(values)
-            winnerIndex = self.getWinnerIndex(rand, values)
-            winnerAddress = distribution[winnerIndex][0]
+            total_value = sum(values)
+            winner_index = self.getWinnerIndex(rand, values)
+            winner_address = distribution[winner_index][0]
 
-        winner = {'distribution': distribution, 'winnerAddress': winnerAddress, 'winnerIndex': winnerIndex, 'random': rand, 'target': totalValue*rand}
+        winner = {'distribution': distribution, 'winnerAddress': winner_address, 'winnerIndex': winner_index, 'random': rand, 'target': total_value*rand}
 
         return winner
-
-
 
     def extractValues(self, distribution):
         values = []
@@ -139,19 +133,16 @@ class Random():
 
         return values
 
-
     def getWinnerIndex(self, rand, values):
 
         choice = 0
         total = sum(values)
 
-        if(total > 0):
+        if total > 0:
             cumulative = 0.0
             for i in range(0, len(values)):
                 cumulative = cumulative + values[i]
-                if (cumulative >= (rand*total) ):
+                if cumulative >= (rand*total):
                     choice = i
                     break
         return choice
-
-
