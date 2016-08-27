bd1eda650bb1db3eb098eb9453e2aa03a7d27cbf
Wouter Glorieux <wouter@valyrian.tech>
2016-08-27 21:00:01 +0200

    pep8

--- BlockInputs/BlockInputs.py
+++ BlockInputs/BlockInputs.py
@@ -4,24 +4,23 @@ from BlockData import BlockData
 from validators import validators as validator
 
 
-
 def SIL(address, block=0):
     response = {'success': 0}
     SIL = []
 
     if validator.validAddress(address):
         if block == 0:
-            latestBlock = BlockData.latestBlock()
+            latest_block = BlockData.latestBlock()
 
-            if 'success' in latestBlock and latestBlock['success'] == 1:
-                block = latestBlock['latestBlock']['height']
+            if 'success' in latest_block and latest_block['success'] == 1:
+                block = latest_block['latestBlock']['height']
             else:
                 response['error'] = 'Unable to retrieve latest block'
 
-        txsData = BlockData.transactions(address)
-        if 'success' in txsData and txsData['success'] == 1:
-            txs = txsData['TXS']
-            SIL = TXS2SIL(txs, block)
+        txs_data = BlockData.transactions(address)
+        if 'success' in txs_data and txs_data['success'] == 1:
+            txs = txs_data['TXS']
+            SIL = txs_2_SIL(txs, block)
         else:
             response['error'] = 'Unable to retrieve transactions'
 
@@ -35,22 +34,20 @@ def SIL(address, block=0):
     return response
 
 
+def txs_2_SIL(txs, block=0):
 
-
-def TXS2SIL(txs, block=0):
-
-    sortedTXS = sortTXS(txs)
+    sorted_txs = sortTXS(txs)
     SIL = []
-    for tx in sortedTXS:
-        if tx['receiving'] == True and (block == 0 or tx['blockHeight'] <= block) and tx['blockHeight'] != None:
+    for tx in sorted_txs:
+        if tx['receiving'] == True and (block == 0 or tx['blockHeight'] <= block) and tx['blockHeight'] is not None:
             recurring = False
             for i in range(0, len(SIL)):
                 if SIL[i][0] == tx['primeInputAddress']:
                     SIL[i][1] += tx['receivedValue']
                     recurring = True
 
-            if recurring == False:
-                SIL.append( [tx['primeInputAddress'], tx['receivedValue'], 0 ,tx['blockHeight'] ] )
+            if not recurring:
+                SIL.append([tx['primeInputAddress'], tx['receivedValue'], 0, tx['blockHeight']])
 
     total = float(totalReceived(SIL))
 
@@ -59,6 +56,7 @@ def TXS2SIL(txs, block=0):
 
     return SIL
 
+
 def totalReceived(SIL):
     total = 0
     for tx_input in SIL:
@@ -66,20 +64,18 @@ def totalReceived(SIL):
 
     return total
 
+
 def sortTXS(txs):
-    blockTXS = {}
+    block_txs = {}
     for tx in txs:
-        if tx['blockHeight'] in blockTXS:
-            blockTXS[tx['blockHeight']].append(tx)
+        if tx['blockHeight'] in block_txs:
+            block_txs[tx['blockHeight']].append(tx)
         else:
-            blockTXS[tx['blockHeight']] = [tx]
+            block_txs[tx['blockHeight']] = [tx]
 
-    sortedTXS = []
-    for block in sorted(blockTXS):
-        for tx in sorted(blockTXS[block], key= lambda x: x['txid']):
-            sortedTXS.append(tx)
+    sorted_txs = []
+    for block in sorted(block_txs):
+        for tx in sorted(block_txs[block], key=lambda x: x['txid']):
+            sorted_txs.append(tx)
 
-    return sortedTXS
-
-
-
+    return sorted_txs
