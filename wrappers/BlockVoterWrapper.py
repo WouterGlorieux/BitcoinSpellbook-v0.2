d86cc917e81b4ed22040df4bd83c530a1ee3c8a2
Wouter Glorieux <wouter@valyrian.tech>
2016-08-28 10:03:21 +0200

    pep8

--- wrappers/BlockVoterWrapper.py
+++ wrappers/BlockVoterWrapper.py
@@ -7,44 +7,43 @@ import json
 import urllib
 
 
-
 class BlockVoterWrapper():
     def __init__(self, url):
         self.url = url
 
-
-    def proposal(self, address, proposal, options=[], voteCost=0, weights='', registrationAddress="", registrationBlockHeight=0, registrationXPUB=""):
+    def proposal(self, address, proposal, options=None, vote_cost=0, weights='', registration_address="",
+                 registration_block_height=0, registration_xpub=""):
+        if not options:
+            options = []
         response = {'success': 0}
-        parameters = {}
-        parameters['address'] = address
-        parameters['proposal'] = proposal
+        parameters = {'address': address,
+                      'proposal': proposal}
 
-        if options != []:
-            strOptions = ""
+        if options:
+            str_options = ""
             for option in options:
-                strOptions += option + "|"
+                str_options += option + "|"
 
-            strOptions = strOptions[:-1]
-            parameters['options'] = strOptions
+            str_options = str_options[:-1]
+            parameters['options'] = str_options
 
-        if voteCost != 0:
-            parameters['voteCost'] = str(voteCost)
+        if vote_cost != 0:
+            parameters['vote_cost'] = str(vote_cost)
 
         if weights != '':
             parameters['weights'] = weights
 
-        if registrationAddress != "":
-            parameters['regAddress'] = str(registrationAddress)
+        if registration_address != "":
+            parameters['regAddress'] = str(registration_address)
 
-        if registrationBlockHeight != 0:
-            parameters['regBlockHeight'] = str(registrationBlockHeight)
+        if registration_block_height != 0:
+            parameters['regBlockHeight'] = str(registration_block_height)
 
-        if registrationXPUB != "":
-            parameters['regXPUB'] = str(registrationXPUB)
+        if registration_xpub != "":
+            parameters['regXPUB'] = str(registration_xpub)
 
-
-        queryString  = urllib.urlencode(parameters)
-        url = self.url + "/voter/proposal?" + queryString
+        query_string = urllib.urlencode(parameters)
+        url = self.url + "/voter/proposal?" + query_string
 
         try:
             ret = urllib2.urlopen(urllib2.Request(url))
@@ -54,43 +53,44 @@ class BlockVoterWrapper():
 
         return response
 
-    def results(self, address, proposal, options=[], voteCost=0, blockHeight=0, weights='Equal', registrationAddress="", registrationBlockHeight=0, registrationXPUB=""):
+    def results(self, address, proposal, options=None, vote_cost=0, block_height=0, weights='Equal',
+                registration_address="", registration_block_height=0, registration_xpub=""):
+        if not options:
+            options = []
         response = {'success': 0}
-        parameters = {}
-        parameters['address'] = address
+        parameters = {'address': address}
 
         if proposal != "":
             parameters['proposal'] = str(proposal)
 
-        if options != []:
-            strOptions = ""
+        if options:
+            str_options = ""
             for option in options:
-                strOptions += option + "|"
+                str_options += option + "|"
 
-            strOptions = strOptions[:-1]
-            parameters['options'] = strOptions
+            str_options = str_options[:-1]
+            parameters['options'] = str_options
 
-        if voteCost != 0:
-            parameters['voteCost'] = str(voteCost)
+        if vote_cost != 0:
+            parameters['vote_cost'] = str(vote_cost)
 
         if weights != '':
             parameters['weights'] = weights
 
-        if blockHeight != 0:
-            parameters['block_height'] = str(blockHeight)
+        if block_height != 0:
+            parameters['block_height'] = str(block_height)
 
-        if registrationAddress != "":
-            parameters['regAddress'] = str(registrationAddress)
+        if registration_address != "":
+            parameters['regAddress'] = str(registration_address)
 
-        if registrationBlockHeight != 0:
-            parameters['regBlock'] = str(registrationBlockHeight)
+        if registration_block_height != 0:
+            parameters['regBlock'] = str(registration_block_height)
 
-        if registrationXPUB != "":
-            parameters['regXPUB'] = str(registrationXPUB)
+        if registration_xpub != "":
+            parameters['regXPUB'] = str(registration_xpub)
 
-
-        queryString  = urllib.urlencode(parameters)
-        url = self.url + "/voter/results?" + queryString
+        query_string = urllib.urlencode(parameters)
+        url = self.url + "/voter/results?" + query_string
 
         try:
             ret = urllib2.urlopen(urllib2.Request(url))
