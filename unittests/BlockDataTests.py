41a8a38c406e1f40b5731da79393756d59385805
Wouter Glorieux <wouter@valyrian.tech>
2016-08-30 22:07:54 +0200

    pep8

--- unittests/BlockDataTests.py
+++ unittests/BlockDataTests.py
@@ -5,31 +5,27 @@ PROVIDERS = ['Blocktrail.com', 'Blockchain.info', 'Blockexplorer.com']
 from wrappers import SpellbookWrapper
 
 
-logging.basicConfig(filename='unittests.log',level=logging.DEBUG)
+logging.basicConfig(filename='unittests.log', level=logging.DEBUG)
+
 
 class BlockDataTests(unittest.TestCase):
 
-    def test_BlockData_GenesisBlock(self):
+    def test_blockdata_genesisblock(self):
         api = SpellbookWrapper.SpellbookWrapper().BlockData()
         for provider in PROVIDERS:
             data = api.block(0, provider)
 
             self.assertIn('success', data, provider)
             if 'success' in data and data['success'] == 1:
-                genesisBlock = data['block']
-                self.assertEqual(genesisBlock['hash'], "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f")
-                self.assertEqual(genesisBlock['merkleroot'], "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b")
-                self.assertEqual(genesisBlock['size'], 285)
-                self.assertEqual(genesisBlock['height'], 0)
-                self.assertEqual(genesisBlock['time'], 1231006505)
+                genesis_block = data['block']
+                self.assertEqual(genesis_block['hash'],
+                                 "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f")
+                self.assertEqual(genesis_block['merkleroot'],
+                                 "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b")
+                self.assertEqual(genesis_block['size'], 285)
+                self.assertEqual(genesis_block['height'], 0)
+                self.assertEqual(genesis_block['time'], 1231006505)
 
             else:
                 logging.error(data['error'])
                 self.fail('Unable to retrieve genesisBlock from ' + provider)
-
-
-
-
-
-
-
