23470dfece00d654fe665785ceb6064b5612f0d3
Wouter Glorieux <wouter@valyrian.tech>
2016-08-31 22:04:23 +0200

    pep8

--- BlockData/blockexplorers/Blockchain_info.py
+++ BlockData/blockexplorers/Blockchain_info.py
@@ -9,23 +9,24 @@ from pprint import pprint
 
 API_VERSION = 'v1'
 
+
 class API:
     def __init__(self, key='', secret=''):
         self.key = key
         self.secret = secret
 
-    def getTXS(self, address):
+    def get_txs(self, address):
         response = {'success': 0}
-        LIMIT = 50 #max number of tx given by blockchain.info is 50
+        limit = 50  # max number of tx given by blockchain.info is 50
 
         latest_block_height = -1
         try:
-            latest_block_height = self.getLatestBlock()['latestBlock']['height']
+            latest_block_height = self.get_latest_block()['latestBlock']['height']
         except:
             logging.warning('Blockchain.info: unable to retrieve latest block')
             response = {'success': 0, 'error': 'unable to retrieve latest block'}
 
-        url = 'https://blockchain.info/address/' + address + '?format=json&limit=' + str(LIMIT)
+        url = 'https://blockchain.info/address/' + address + '?format=json&limit=' + str(limit)
         try:
             ret = urllib2.urlopen(urllib2.Request(url))
             data = json.loads(ret.read())
@@ -33,13 +34,13 @@ class API:
             data = {}
 
         transactions = data['txs']
-        nTx = data['n_tx']
+        n_tx = data['n_tx']
 
         if 'n_tx' in data:
             n_tx = data['n_tx']
-            if n_tx > LIMIT:
-                for i in range(1, int(n_tx/LIMIT)+1):
-                    url = 'https://blockchain.info/address/' + address + '?format=json&limit=' + str(LIMIT) + '&offset=' + str(LIMIT*i)
+            if n_tx > limit:
+                for i in range(1, int(n_tx/limit)+1):
+                    url = 'https://blockchain.info/address/' + address + '?format=json&limit=' + str(limit) + '&offset=' + str(limit*i)
                     try:
                         ret = urllib2.urlopen(urllib2.Request(url))
                         data = json.loads(ret.read())
@@ -61,10 +62,9 @@ class API:
                 tx.block_height = None
                 tx.confirmations = 0
 
-            for input in transaction['inputs']:
-                tx_in = {}
-                tx_in['address'] = input['prev_out']['addr']
-                tx_in['value'] = input['prev_out']['value']
+            for tx_input in transaction['inputs']:
+                tx_in = {'address': tx_input['prev_out']['addr'],
+                         'value': tx_input['prev_out']['value']}
                 tx.inputs.append(tx_in)
 
             for out in transaction['out']:
@@ -77,24 +77,24 @@ class API:
                     if out['script'][:2] == '6a':
                         tx_out['OP_RETURN'] = TxFactory.decodeOP_RETURN(out['script'])
 
-
                 tx_out['value'] = out['value']
                 tx_out['spent'] = out['spent']
                 tx.outputs.append(tx_out)
 
             txs.insert(0, tx.to_dict(address))
 
-        if nTx != len(txs):
-            logging.warning('Blockchain.info: Warning: not all transactions are retrieved! ' + str(len(txs)) + ' of ' +  str(nTx))
-            response = {'success': 0, 'error': 'Not all transactions are retrieved ' + str(len(txs)) + ' of ' +  str(nTx)}
+        if n_tx != len(txs):
+            logging.warning('Blockchain.info: Warning: not all transactions are retrieved! ' + str(len(txs)) + ' of ' + str(n_tx))
+            response = {'success': 0, 'error': 'Not all transactions are retrieved ' + str(len(txs)) + ' of ' + str(n_tx)}
         else:
             response = {'success': 1, 'TXS': txs}
 
         return response
 
-    def getLatestBlock(self):
+    @staticmethod
+    def get_latest_block():
         response = {'success': 0}
-        latestBlock = {}
+        latest_block = {}
         data = {}
         url = 'https://blockchain.info/nl/latestblock'
         try:
@@ -105,28 +105,29 @@ class API:
             response = {'success': 0, 'error': 'unable to retrieve latest block'}
 
         if 'height' in data:
-            latestBlock['height'] = data['height']
-            latestBlock['hash'] = data['hash']
-            latestBlock['time'] = data['time']
+            latest_block['height'] = data['height']
+            latest_block['hash'] = data['hash']
+            latest_block['time'] = data['time']
 
-            url = 'https://blockchain.info/nl/rawblock/' + latestBlock['hash']
+            url = 'https://blockchain.info/nl/rawblock/' + latest_block['hash']
             data = {}
             try:
                 ret = urllib2.urlopen(urllib2.Request(url))
                 data = json.loads(ret.read())
             except:
-                logging.warning('Blockchain.info: unable to retrieve block ' + str(latestBlock['height']))
-                response = {'success': 0, 'error': 'unable to retrieve block ' + str(latestBlock['height'])}
+                logging.warning('Blockchain.info: unable to retrieve block ' + str(latest_block['height']))
+                response = {'success': 0, 'error': 'unable to retrieve block ' + str(latest_block['height'])}
 
-            if 'hash' in data and data['hash'] == latestBlock['hash']:
-                latestBlock['merkleroot'] = data['mrkl_root']
-                latestBlock['size'] = data['size']
+            if 'hash' in data and data['hash'] == latest_block['hash']:
+                latest_block['merkleroot'] = data['mrkl_root']
+                latest_block['size'] = data['size']
 
-            response = {'success': 1, 'latestBlock': latestBlock}
+            response = {'success': 1, 'latestBlock': latest_block}
 
         return response
 
-    def getBlock(self, height):
+    @staticmethod
+    def get_block(height):
         response = {'success': 0}
         block = {}
         data = {}
@@ -142,7 +143,7 @@ class API:
             blocks = data['blocks']
 
             for i in range(0, len(blocks)):
-                if blocks[i]['main_chain'] == True and blocks[i]['height'] == height:
+                if blocks[i]['main_chain'] is True and blocks[i]['height'] == height:
                     block['height'] = blocks[i]['height']
                     block['hash'] = blocks[i]['hash']
                     block['time'] = blocks[i]['time']
@@ -154,7 +155,7 @@ class API:
         return response
 
     #(Multiple addresses divided by |)
-    def getBalances(self, addresses):
+    def get_balances(self, addresses):
         response = {'success': 0}
         self.error = ''
         data = {}
@@ -167,8 +168,6 @@ class API:
             logging.warning('Blockchain.info: unable to retrieve data for addresses ' + addresses)
             self.error = 'Unable to retrieve data for addresses ' + addresses
 
-
-
         if 'addresses' in data:
             for i in range(0, len(data['addresses'])):
                 address = data['addresses'][i]
@@ -177,32 +176,30 @@ class API:
                 balances[address['address']]['received'] = address['total_received']
                 balances[address['address']]['sent'] = address['total_sent']
 
-        #reverse unconfirmed transactios for security reasons
+        #reverse unconfirmed transactions for security reasons
         if 'txs' in data:
             for i in range(0, len(data['txs'])):
                 tx = data['txs'][i]
                 if 'block_height' not in tx:
                     for address in addresses.split('|'):
-                        sendingTX = False
-                        receivingTX = False
+                        is_sending_tx = False
+                        is_receiving_tx = False
 
                         for j in range(0, len(tx['inputs'])):
                             if tx['inputs'][j]['prev_out']['addr'] == address:
-                                sendingTX = True
+                                is_sending_tx = True
 
-                        if sendingTX == True:
+                        if is_sending_tx:
                             balances[address]['sent'] += tx['result']
                             balances[address]['balance'] -= tx['result']
                         else:
                             for j in range(0, len(tx['out'])):
                                 if tx['out'][j]['addr'] == address:
-                                    receivingTX = True
+                                    is_receiving_tx = True
 
-                            if receivingTX == True:
+                            if is_receiving_tx:
                                 balances[address]['received'] -= tx['result']
                                 balances[address]['balance'] -= tx['result']
-
-
 
         if self.error == '':
             response = {'success': 1, 'balances': balances}
@@ -211,7 +208,7 @@ class API:
 
         return response
 
-    def getPrimeInputAddress(self, txid):
+    def get_prime_input_address(self, txid):
         url = 'https://blockchain.info/nl/rawtx/' + str(txid)
         data = {}
         response = {'success': 0}
@@ -222,27 +219,25 @@ class API:
             logging.warning('Blockchain.info: unable to retrieve prime input address of tx ' + txid)
             response = {'success': 0, 'error': 'unable to retrieve prime input address of tx ' + txid}
 
-
-        tx_inputs = []
         if 'hash' in data and data['hash'] == txid:
             tx_inputs = data['inputs']
 
-            inputAddresses = []
+            input_addresses = []
             for i in range(0, len(tx_inputs)):
-                inputAddresses.append(tx_inputs[i]['prev_out']['addr'])
+                input_addresses.append(tx_inputs[i]['prev_out']['addr'])
 
-            primeInputAddress = []
-            if len(inputAddresses) > 0:
-                primeInputAddress = sorted(inputAddresses)[0]
+            prime_input_address = []
+            if len(input_addresses) > 0:
+                prime_input_address = sorted(input_addresses)[0]
 
-            response = {'success': 1, 'PrimeInputAddress': primeInputAddress}
+            response = {'success': 1, 'PrimeInputAddress': prime_input_address}
 
         return response
 
     #Multiple bitcoin addresses, separated by "|"
-    def getUTXOs(self, addresses, confirmations=3):
+    def get_utxos(self, addresses, confirmations=3):
         response = {'success': 0}
-        UTXOs = []
+        utxos = []
         url = 'https://blockchain.info/nl/unspent?active=' + addresses
         data = {}
         try:
@@ -252,17 +247,15 @@ class API:
             logging.warning('Blockchain.info: unable to retrieve utxos')
             response = {'success': 0, 'error': 'unable to retrieve utxos'}
 
-
         if 'unspent_outputs' in data:
             for i in range(0, len(data['unspent_outputs'])):
-                utxo = {}
-
-                utxo['confirmations'] = int(data['unspent_outputs'][i]['confirmations'])
-                utxo['output'] = data['unspent_outputs'][i]['tx_hash_big_endian'] + ":" + str(data['unspent_outputs'][i]['tx_output_n'])
-                utxo['value'] = data['unspent_outputs'][i]['value']
+                utxo = {'confirmations': int(data['unspent_outputs'][i]['confirmations']),
+                        'output': data['unspent_outputs'][i]['tx_hash_big_endian'] + ":" + str(
+                            data['unspent_outputs'][i]['tx_output_n']),
+                        'value': data['unspent_outputs'][i]['value']}
 
                 if utxo['confirmations'] >= confirmations:
-                    UTXOs.append(utxo)
+                    utxos.append(utxo)
 
                 url = 'https://blockchain.info/nl/rawtx/' + data['unspent_outputs'][i]['tx_hash_big_endian']
                 tx = {}
@@ -279,7 +272,5 @@ class API:
                         if tx_output['n'] == data['unspent_outputs'][i]['tx_output_n']:
                             utxo['address'] = tx_output['addr']
 
-
-
-            response = {'success': 1, 'UTXOs': UTXOs}
+            response = {'success': 1, 'UTXOs': utxos}
         return response
