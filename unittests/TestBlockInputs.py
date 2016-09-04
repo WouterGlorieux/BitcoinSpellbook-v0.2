d47b4aee893e5cb781dcd63769164cc1d382a97b
Wouter Glorieux <wouter@valyrian.tech>
2016-09-04 19:48:12 +0200

    added TestBlockInputs

---
+++ unittests/TestBlockInputs.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from pprint import pprint
+from os import path
+
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+from wrappers import SpellbookWrapper as SpellbookWrapper
+import testconfig
+
+
+url = testconfig.url
+key = testconfig.key
+secret = testconfig.secret
+
+#test parameters
+address = '1Robbk6PuJst6ot6ay2DcVugv8nxfJh5y'
+block_height = 400000
+
+api = SpellbookWrapper.SpellbookWrapper(url).blockinputs()
+
+#Test most recent SIL
+pprint(api.get_sil(address))
+
+#Test SIL at specified block_height
+pprint(api.get_sil(address, block_height))
